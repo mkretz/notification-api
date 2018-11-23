65f7b362817e4457c3a2f218f7e525d48876a11b
Mathis Kretz <mathis.kretz@swisscom.com>
2018-11-23 22:42:25 +0100

    add a log line after saving notification and reformat

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-var restify = require('restify');
-var bodyParser = require('restify-plugins').bodyParser;
+var restify = require("restify");
+var bodyParser = require("restify-plugins").bodyParser;
 var server = restify.createServer();
-var cfenv = require('cfenv');
-var appEnv = cfenv.getAppEnv()
-var packageJson = require('./package.json');
-var mongoose = require('mongoose');
-var Notification = require('./notification/model.js').Notification;
-var corsMiddleware = require('restify-cors-middleware')
-var amqp = require('amqplib');
+var cfenv = require("cfenv");
+var appEnv = cfenv.getAppEnv();
+var packageJson = require("./package.json");
+var mongoose = require("mongoose");
+var Notification = require("./notification/model.js").Notification;
+var corsMiddleware = require("restify-cors-middleware");
+var amqp = require("amqplib");
 
-function sendJsonMessage (message) {
-    return amqp.connect(appEnv.getServiceURL('notification-mq'))
-        .then(function (conn) {
-            return conn.createChannel()
-                .then(function (ch) {
-                    return ch.assertQueue("notifications", {durable: false})
-                        .then(function () {
-                            ch.sendToQueue("notifications", Buffer.from(JSON.stringify(message)));
-                            return ch.close();
-                        })
-                        .catch(console.warn);
-                })
-                .finally(function () {
-                    return conn.close();
-                })
-
+function sendJsonMessage(message) {
+  return amqp
+    .connect(appEnv.getServiceURL("notification-mq"))
+    .then(function(conn) {
+      return conn
+        .createChannel()
+        .then(function(ch) {
+          return ch
+            .assertQueue("notifications", { durable: false })
+            .then(function() {
+              ch.sendToQueue(
+                "notifications",
+                Buffer.from(JSON.stringify(message))
+              );
+              return ch.close();
+            })
+            .catch(console.warn);
         })
-        .catch(console.warn)
+        .finally(function() {
+          return conn.close();
+        });
+    })
+    .catch(console.warn);
 }
 
 var cors = corsMiddleware({
-  origins: ['*'],
+  origins: ["*"],
   allowHeaders: [],
   exposeHeaders: []
 });
@@ -40,47 +45,48 @@ server.use(cors.actual);
 
 server.use(bodyParser());
 
-server.get('/', function (req,res,next) {
-    var apiInfo = {
-        name : packageJson.name,
-        version : packageJson.version
-    };
-    return res.send(apiInfo);
-})
+server.get("/", function(req, res, next) {
+  var apiInfo = {
+    name: packageJson.name,
+    version: packageJson.version
+  };
+  return res.send(apiInfo);
+});
 
-server.get('/notification', function (req,res,next) {
-    Notification
-        .find()
-        .exec()
-        .then(function (notifications) {
-            return res.send(notifications);
-        })
-})
+server.get("/notification", function(req, res, next) {
+  Notification.find()
+    .exec()
+    .then(function(notifications) {
+      return res.send(notifications);
+    });
+});
 
-server.post('/notification', function (req,res,next) {
-    var notification = new Notification();
-    notification.text = req.body.text;
-    notification.save()
-        .then(function (savedNotification) {
-            sendJsonMessage(savedNotification);
-            return res.send(204,savedNotification);
-        })
-})
+server.post("/notification", function(req, res, next) {
+  var notification = new Notification();
+  notification.text = req.body.text;
+  notification.save().then(function(savedNotification) {
+    console.log("Saved notification " + savedNotification.text);
+    sendJsonMessage(savedNotification);
+    return res.send(204, savedNotification);
+  });
+});
 
-server.del('/notification/:notificationid', function (req,res,next) {
-    Notification.findByIdAndRemove(req.params.notificationid, function (removedNotification) {
-        return res.send(204);
-    })
-})
+server.del("/notification/:notificationid", function(req, res, next) {
+  Notification.findByIdAndRemove(req.params.notificationid, function(
+    removedNotification
+  ) {
+    return res.send(204);
+  });
+});
 
 if (process.env.LOADER_IO_KEY) {
-    server.get('/' + process.env.LOADER_IO_KEY, function (req,res,next) {
-        res.setHeader('content-type', 'text/plain');
-        return res.send(process.env.LOADER_IO_KEY);
-    })
+  server.get("/" + process.env.LOADER_IO_KEY, function(req, res, next) {
+    res.setHeader("content-type", "text/plain");
+    return res.send(process.env.LOADER_IO_KEY);
+  });
 }
 
-mongoose.connect(appEnv.getServiceURL('notification-db'));
-server.listen(appEnv.port,appEnv.bind, function () {
-    console.log('server listening at %s on port %s', appEnv.bind, appEnv.port);
+mongoose.connect(appEnv.getServiceURL("notification-db"));
+server.listen(appEnv.port, appEnv.bind, function() {
+  console.log("server listening at %s on port %s", appEnv.bind, appEnv.port);
 });
